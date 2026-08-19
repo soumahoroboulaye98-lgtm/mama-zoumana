b3ca77b2a33e85612beccaf7479aa483973f9f47
soumahoroboulaye98-lgtm <soumahoroboulaye98@gmail.com>
2026-08-19 13:50:31 +0000

    Ameliore config SSL Neon

--- db.js
+++ db.js
@@ -1,9 +1,13 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+
 module.exports = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
+  ssl: {
+    rejectUnauthorized: true,
+    minVersion: 'TLSv1.2'
+  },
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 5000,
   max: 20,
